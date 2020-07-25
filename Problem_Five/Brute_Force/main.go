b76aaf1a4e83db7c47f18ebd872e4e8b272c998f
Amaterasus <skyetay03@googlemail.com>
2020-07-25 17:43:43 +0100

    project number 5 brute force working and timed

---
+++ Problem_Five/Brute_Force/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	answer := lcm(1, 20)
+	t := time.Now()
+	fmt.Println("The answer is", answer)
+
+	elapsed := t.Sub(start)	
+	fmt.Println("Time taken", elapsed)
+}
+
+func lcm(lowestNumber, highestNumber int) int {
+	numbers := makeRange(lowestNumber, highestNumber)
+	flip := true
+	i := highestNumber
+	for flip {
+		if bruteForce(i, numbers) {
+			flip = false
+			break
+		}
+		i += highestNumber
+	}
+	return i
+}
+
+func bruteForce(testNumber int, numberRange []int) bool {
+
+	for _ , v := range numberRange {
+		if !divisible(testNumber, v) {
+			return false
+		}
+	}
+	return true
+}
+
+func divisible(testNumber, divider int) bool {
+	if (testNumber % divider) == 0 {
+		return true
+	}
+	return false
+}
+
+func makeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
